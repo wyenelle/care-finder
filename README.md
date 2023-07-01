b3efed9f4f586f564f325746f11fc7f4e5f53062
wyenelle <favourkcb@gmail.com>
2023-07-01 12:58:14 -0700

    feat: added a readme file

---
+++ README.md
@@ -0,0 +1,1 @@
+#This Project is far from completed..... watch out!!!
--- src/pages/hospitals/hospital.tsx
+++ src/pages/hospitals/hospital.tsx
@@ -2,46 +2,24 @@ import {useContext, useState} from 'react'
 import MyContext from '../../context/context'
 
 const Hospital = () => {
-    const {hospitalsList,deleteHospital,getHospital,updateHospitalData,setUpdatedInfo,updatedInfo,setHospitalList
+    const {hospitalsList,deleteHospital,getHospital,updateHospitalData,setUpdatedInfo,updatedInfo
     } = useContext(MyContext)
+
      const [location,setLocation] = useState('')
-     const [showError,setShowError] = useState(false)
+
+    //  deletes an hospital from the collection by colleccting it's id here and passing it up to th deleting function
     const del = (id) => {
         deleteHospital(id)
         getHospital()
     }
-    
+
+    //  hndles editing the hospital data
     const handleInput = (e) => {
         const {value} = e.target
         setUpdatedInfo({...updatedInfo,hospital: value})
     }
 
-    const handleSearch = (e) => {
-        const {value} = e.target
-        setLocation(value)
-        
-        const data = [...hospitalsList]
-        setHospitalList(data);
-        
-        // console.log(data)
- 
-        // if(data.length >= 1){
-        //     setHospitalList(data);
-        //     console.log(data);
-            
-        //     // setHospitalList(data)
-        // }else if (e.target.value === ''){
-        //     getHospital()
-        //     setHospitalList(hospitalsList)    
-        // }
-        // else if(data === []){
-        //     console.log(false)
-        //     setShowError(true)
-        // }
-        
-    }
-    // console.log(location);
-
+    const filteredHospitalData = hospitalsList.filter(hospital => hospital.state.toLowerCase().includes(location.toLocaleLowerCase()))
   return (
     <div>
         <input type='text' list='locations' name='location' onChange={e => setLocation(e.target.value)}/>
@@ -51,10 +29,13 @@ const Hospital = () => {
                 <option value="abujas"></option>
                 <option value="abia"></option>
             </datalist>
-            {/* <button style={{padding : '10px'}} onClick={handleSearch}>search</button> */}
+
         <h1>hospitals</h1>
+        
         {
-            hospitalsList.filter(hospital => hospital.state.toLowerCase().includes(location.toLocaleLowerCase())).map((obj) => (
+            // filters the state and maps over the returned array as well
+            filteredHospitalData.length >= 1 ?
+            filteredHospitalData.map((obj) => (
                 <div key={obj.id}>
                     <h2>{obj.name} </h2>
                     <span>{`${obj.address} ${obj.state}` } </span>
@@ -63,12 +44,9 @@ const Hospital = () => {
                     <input type='text' onChange={handleInput}/>
                     <button style={{padding : '10px'}} onClick={() => updateHospitalData(obj.id)}>edit</button>
                 </div>
-            ))
+            )) : <h3> no result for this search</h3>
         }
-        {
-            showError && <h2>no result ffor this searcgh</h2>
-
-        }
+        
     </div>
   )
 }
