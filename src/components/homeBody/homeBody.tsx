a37a3bf8aaff3a50606585e7fcc47b55471c81a3
Favour  Timothy <favourkcb@gmail.com>
2023-07-11 11:05:27 -0700

    redesigning care fider

--- src/components/homeBody/homeBody.tsx
+++ src/components/homeBody/homeBody.tsx
@@ -1,29 +1,25 @@
-import styles from './home.module.css'
-import helper from '../../helper/helper'
+import styles from "./home.module.css";
+import helper from "../../helper/helper";
 
 const HomeBody = () => {
   return (
     <section className={styles.container}>
-      <div className={styles.text}>
+      <section className={styles.text}>
         <div>
-        <h2>
-          we are number one when it comes to healthcare services
-        </h2>
+          <h2>we are number one when it comes to healthcare services</h2>
         </div>
-      </div>
-      <div className={styles.img}>
-            <img src={helper.Hospital3} alt="" className={styles.image}/>
-        <div className={styles.imgContainer}>
-            <div className={styles.box}> 
-            {/* <img src={helper.Hospital2} alt="" className={styles.img2}/> */}
-            </div>
-            <div className={styles.box}>
-          {/* <img src={helper.Hospital1} alt="" className={styles.img2}/> */}
-            </div>
-        </div>
-      </div>
-    </section>
-  )
-}
+      </section>
 
-export default HomeBody
+      <section className={styles.img}>
+        <div className={styles.box}>
+        <img src={helper.DoctorWithPatient} alt="" className={styles.image} />
+        </div>
+        <div className={styles.box2}>
+          <img src={helper.Gloves} alt="" className={styles.img2} />
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default HomeBody;
--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -4,12 +4,16 @@ import Doctors from '../assets/doctors.jpg'
 import Hospital1 from '../assets/hospital1.jpg'
 import Hospital2 from '../assets/hospital2.jpg'
 import Hospital3 from '../assets/hospital3.jpg'
+import DoctorWithPatient from '../assets/doctor-with-patient.jpg'
+import Gloves from '../assets/gloves.jpg'
 
 export default {
+    DoctorWithPatient,
+    Gloves,
     Hospital,
     Doctor,
     Doctors,
     Hospital1,  
     Hospital2,
     Hospital3
-}
+}   
